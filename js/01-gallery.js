6fc1fb5cc36e1f63b94e608833e51e1192112b54
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-05-19 22:39:22 +0300

    add code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryRefs = document.querySelector('.gallery');
+const newElement = createElement(galleryItems);
+galleryRefs.addEventListener('click', originImgClick);
+galleryRefs.insertAdjacentHTML('beforeend', newElement);
+function createElement(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" rel="nofollov">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("");
+};
+function originImgClick(e) {
+    // e.preventDefoult();
+    console.log(e);
+    console.log(e.target.classList.contains('gallery__image'));
+    if (!e.target.classList.contains('gallery__image')) {
+        return
+    } else {
+        const instance = basicLightbox.create(`
+	<img
+      src="${e.target.dataset.source}"
+    />
+`);
+        instance.show()
+    }
+    
+    
+}
+// instance.show()
+originImgClick()
 
-console.log(galleryItems);
+
+
+// console.log(newElement);
