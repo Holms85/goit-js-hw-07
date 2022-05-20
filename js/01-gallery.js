71234ee3bbbb3e99519006014ce26dfe02f0096c
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-05-20 18:07:49 +0300

    change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,13 @@
-import { galleryItems } from './gallery-items.js';
-const galleryRefs = document.querySelector('.gallery');
+import { galleryItems } from "./gallery-items.js";
+const galleryRefs = document.querySelector(".gallery");
 const newElement = createElement(galleryItems);
-galleryRefs.addEventListener('click', originImgClick);
-galleryRefs.insertAdjacentHTML('beforeend', newElement);
+
+galleryRefs.insertAdjacentHTML("beforeend", newElement);
 function createElement(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}" rel="nofollov">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -14,29 +15,26 @@ function createElement(galleryItems) {
       alt="${description}"
     />
   </a>
-</div>`
-    }).join("");
-};
-function originImgClick(e) {
-    // e.preventDefoult();
-    console.log(e);
-    console.log(e.target.classList.contains('gallery__image'));
-    if (!e.target.classList.contains('gallery__image')) {
-        return
-    } else {
-        const instance = basicLightbox.create(`
+</div>`;
+    })
+    .join("");
+}
+const originImgClick = (e) => {
+  e.preventDefoult();
+  console.log(e);
+  // console.log(e.target.classList.contains('.gallery__image'));
+  const targetElement = e.target.classList.contains("gallery__image");
+  if (!targetElement) {
+    return;
+  } else {
+    const instance = basicLightbox.create(`
 	<img
       src="${e.target.dataset.source}"
+      alt="${description}"
     />
 `);
-        instance.show()
-    }
-    
-    
-}
-// instance.show()
-originImgClick()
+    instance.show();
+  }
+};
+galleryRefs.addEventListener("click", originImgClick);
 
-
-
-// console.log(newElement);
